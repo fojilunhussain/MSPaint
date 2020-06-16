574cbb0e81c2304b18404d77b776133373302c38
Fojilun Hussain <fojilun.hussain@gmail.com>
2020-06-16 08:49:45 +0100

    Revert "implemented imgur api to get images"
    This reverts commit d2f1a8c16e5fb436c3dfb73a276e8402563dce12.

--- MSPaint.js
+++ MSPaint.js
@@ -14,9 +14,8 @@ $(document).ready(function () {
 
     penWidth = $("#brushSizeSlider").value;
 
-    retrieveCanvas(canvas);
-
     canvas.addEventListener("mousemove", function (e) {
+        console.log(event.type);
         findCoord("move", e)
     }, false);
     canvas.addEventListener("mousedown", function (e) {
@@ -24,12 +23,16 @@ $(document).ready(function () {
         findCoord("down", e)
     }, false);
     canvas.addEventListener("mouseup", function (e) {
+        console.log(event.type);
         findCoord("up", e)
     }, false);
     canvas.addEventListener("mouseout", function (e) {
+        console.log(event.type);
         findCoord("out", e)
     }, false);
 
+    retrieveCanvas(canvas);
+    
     $("#brushSizeSlider").change(function (event) {
         console.log("PEN WIDTH: " + this.value);
         penWidth = this.value;
@@ -42,48 +45,23 @@ $(document).ready(function () {
     $("#btnUndoStroke").click(function (event) {
 
     });
-
+    
     $("#btnRedoStroke").click(function (event) {
 
     });
-
+    
     $("#btnClearCanvas").click(function (event) {
         var confirmClear = confirm("Clear canvas?");
         if (confirmClear) {
             ctx.clearRect(0, 0, width, height);
         }
-
+    
         canvas = document.getElementById("canvasSurface");
         storeCanvas(canvas);
     });
 
     $("#btnSaveCanvas").click(function (event) {
 
-    });
-
-    $("#btnImgSearch").click(function () {
-        const imgQuery = $("#inputImgSearch").val();
-        console.log("Image query: " + imgQuery);
-        $.ajax({
-                url: `https://api.imgur.com/3/gallery/search/?q=${imgQuery}`,
-                headers: {
-                    'Authorization': 'Client-ID af79a73753ebd81'
-                },
-                type: "GET",
-                dataType: "json",
-                success: function (response) {
-                    console.log("success");
-                }
-            })
-            .done(function (reponse) {
-                getRandomImage(reponse);
-            })
-            .fail(function(response) {
-                console.log(response.status);
-                $("#imgResult").html(`
-                    <p>oh no</p>
-                `);
-            });
     });
 });
 
@@ -138,7 +116,6 @@ function storeCanvas(canvasSurface) {
         var canvasDataUrl = canvasSurface[0].toDataURL();
         localStorage.setItem("canvasDataUrl", canvasDataUrl);
     } catch (err) {
-        console.log(err)
         var canvasDataUrl = canvasSurface.toDataURL();
         console.log(canvasSurface)
         localStorage.setItem("canvasDataUrl", canvasDataUrl);
@@ -154,18 +131,4 @@ function retrieveCanvas(canvas) {
     canvasImage.onload = function () {
         ctx.drawImage(canvasImage, 0, 0);
     }
-}
-
-function getRandomImage(response) {
-    const numOfAlbumResults = response.data.length;
-
-    const randomAlbumIndex = Math.floor(Math.random() * Math.floor(numOfAlbumResults - 1));
-    const numOfAlbumImageResults = response.data[randomAlbumIndex].images.length;
-    const randomAlbumImageIndex = Math.floor(Math.random() * Math.floor(numOfAlbumImageResults - 1));
-
-    const randomImage = response.data[randomAlbumIndex].images[randomAlbumImageIndex].link;
-
-    $("#imgResult").html(`
-        <img src=${randomImage} width="500" height="500"/>
-    `);
 }
