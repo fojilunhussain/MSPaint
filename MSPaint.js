7d0934d652f13362d66b36e2cead58acbd469ebe
Fojilun Hussain <fojilun.hussain@gmail.com>
2020-07-14 20:21:55 +0100

    added some new error messages

--- MSPaint.js
+++ MSPaint.js
@@ -62,28 +62,51 @@ $(document).ready(function () {
     });
 
     $("#btnImgSearch").click(function () {
+        try {
+            const imageQuery = $("#inputImgSearch").val();
+            console.log("Image : " + imageQuery);
+
+            if (imageQuery == null || imageQuery == undefined || imageQuery.trim() == "") {
+                const imageQuerytype = undefined;
+                throw new imageQueryTypeException(imageQuerytype);
+            }
+
+            // if SFW filter on then throw new error
+        }
+        catch (err) {
+            console.log(err)
+        }
+        finally {
+
+        }
+
         const imgQuery = $("#inputImgSearch").val();
         console.log("Image query: " + imgQuery);
+        console.log(imgQuery);
+
+        console.log(imgQuery.value);
+        (imgQuery == null || imgQuery == undefined || imgQuery.trim() == "") ?
+        $("#imgResult").html(`<p>Can't make an empty search. Please try again</p>`) :
         $.ajax({
-                url: `https://api.imgur.com/3/gallery/search/?q=${imgQuery}`,
-                headers: {
-                    'Authorization': 'Client-ID API_KEY'
-                },
-                type: "GET",
-                dataType: "json",
-                success: function (response) {
-                    console.log("success");
-                }
-            })
-            .done(function (reponse) {
-                getRandomImage(reponse);
-            })
-            .fail(function (response) {
-                console.log(response.status);
-                $("#imgResult").html(`
-                    <p>oh no</p>
-                `);
-            });
+            url: `https://api.imgur.com/3/gallery/search/?q=${imgQuery}`,
+            headers: {
+                'Authorization': 'Client-ID af79a73753ebd81'
+            },
+            type: "GET",
+            dataType: "json",
+            success: function (response) {
+                console.log("success");
+            }
+        })
+        .done(function (reponse) {
+            getRandomImage(reponse);
+        })
+        .fail(function (response) {
+            console.log(response.status);
+            $("#imgResult").html(`
+                <p>Oops! Please try again</p>
+            `);
+        });
     });
 });
 
@@ -156,6 +179,25 @@ function retrieveCanvas(canvas) {
     }
 }
 
+function imageSearchTypeException(imageQuerytype) {
+    this.imageQuerytype = imageQueryType;
+    this.message = "cannot have a blank image query";
+    this.toString = function () {
+        return this.value + this.message;
+    }
+}
+
+// function sfw exception
+// can't search for nsfw images
+
+function imageResponseTypeException(imageResponseType) {
+    this.imageResponseType = imageResponseType;
+    this.message = "is not a jpeg image";
+    this.toString = function () {
+        return this.value + this.message;
+    }
+}
+
 function getRandomImage(response) {
     try {
         const numOfAlbumResults = response.data.length;
@@ -171,11 +213,17 @@ function getRandomImage(response) {
         console.log(`Index of random image chosen: ${randomAlbumImageIndex}`);
 
         const randomImageLink = response.data[randomAlbumIndex].images[randomAlbumImageIndex].link;
-        console.log(`Random image link: ${randomImageLink}`);
 
+        const imageResponseType = response.data[randomAlbumIndex].images[randomAlbumImageIndex].type;
+
+        if (response.data[randomAlbumIndex].images[randomAlbumImageIndex].type != "image/jpeg") {
+            throw new imageResponseTypeException(imageResponseType);
+        }
+
+        console.log(`Random image link: ${randomImageLink}`);
         $("#imgResult").html(`<img src=${randomImageLink} width="500" height="500"/>`);
     } catch (err) {
         console.log(err)
-        $("#imgResult").html(`<p>Oops! Please try again</p>`);
+        $("#imgResult").html(`<p>Make sure you're searching for a valid image. Please try again</p>`);
     }
 }
