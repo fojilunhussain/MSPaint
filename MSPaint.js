50b570072614ad2d13e7e8a503425456ffb1820b
Fojilun Hussain <fojilun.hussain@investec.co.uk>
2020-10-02 23:36:25 +0100

    refactor and remove api key

--- MSPaint.js
+++ MSPaint.js
@@ -1,40 +1,36 @@
-var canvas, ctx, flag = false,
-    prevX = 0,
+var ctx = false,
+    flag = false,
+    canvas = false,
+    isDrawing = false;
+var prevX = 0,
     curX = 0,
     prevY = 0,
-    curY = 0,
-    isDrawing = false,
-    penColour = "black";
+    curY = 0;
+var penColour = "black";
 
 $(document).ready(function () {
-    canvas = document.getElementById("canvasSurface");
+    var canvas = document.getElementById("canvasSurface");
     ctx = canvas.getContext("2d");
-    width = canvas.width;
-    height = canvas.height;
-
+    var width = canvas.width;
+    var height = canvas.height;
     penWidth = $("#brushSizeSlider").value;
 
+    retrieveCanvas();
+
     canvas.addEventListener("mousemove", function (e) {
-        console.log(event.type);
         findCoord("move", e)
     }, false);
     canvas.addEventListener("mousedown", function (e) {
-        console.log(event.type);
         findCoord("down", e)
     }, false);
     canvas.addEventListener("mouseup", function (e) {
-        console.log(event.type);
         findCoord("up", e)
     }, false);
     canvas.addEventListener("mouseout", function (e) {
-        console.log(event.type);
         findCoord("out", e)
     }, false);
-
-    retrieveCanvas(canvas);
     
     $("#brushSizeSlider").change(function (event) {
-        console.log("PEN WIDTH: " + this.value);
         penWidth = this.value;
     });
 
@@ -55,80 +51,112 @@ $(document).ready(function () {
         if (confirmClear) {
             ctx.clearRect(0, 0, width, height);
         }
-    
-        canvas = document.getElementById("canvasSurface");
-        storeCanvas(canvas);
+
+        storeCanvas();
     });
 
     $("#btnSaveCanvas").click(function (event) {
 
     });
-});
 
-function draw() {
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(curX, curY);
-    ctx.strokeStyle = penColour;
-    ctx.lineWidth = penWidth;
-    ctx.stroke();
-    ctx.closePath();
-
-    canvasSurface = $("#canvasSurface");
-    storeCanvas(canvasSurface);
-}
-
-function findCoord(res, e) {
-    if (res == "down") {
-        prevX = curX;
-        prevY = curY;
-        curX = e.clientX - canvas.offsetLeft;
-        curY = e.clientY - canvas.offsetTop;
-
-        flag = true;
-        isDrawing = true;
-        if (isDrawing) {
-            ctx.beginPath();
-            ctx.fillStyle = penColour;
-            ctx.fillRect(curX, curY, 2, 2);
-            ctx.closePath();
-            isDrawing = false;
+    $("#btnImgSearch").click(function () {
+        var imgQuery = $("#inputImgSearch").val();
+        try {
+            console.log("User searched for: " + imgQuery);
+            if (imgQuery == null || imgQuery == undefined || imgQuery.trim() == "") {
+                var imgQueryType = undefined;
+                $("#imgResult").html(`<p>Can't make an empty search. Please try again</p>`)
+                throw new imgQueryTypeException(imgQueryType);
+            }
+            // var imgQuery = $("#inputImgSearch").val();
+            $.ajax({
+                // cache: false,
+                // crossDomain: true,
+                // dataType: 'jsonp',
+                headers: {
+                    'Authorization': 'Client-ID af79a73753ebd81',
+                },
+                success: function (response) {
+                    console.log("success");
+                },
+                // type: "GET",
+                url: `https://api.imgur.com/3/gallery/search/?q=${imgQuery}`
+             })
+            .done(function (response) {
+                getRandomImage(response);
+            })
+            .fail(function (response) {
+                console.log(response.status);
+                $("#imgResult").html(`
+                    <p>Oops! Please try again</p>
+                `);
+            });
         }
-    }
+        catch (err) {
+            console.log(err)
+        }
+        finally {
 
-    if (res == "up" || res == "out") {
-        flag = false;
-    }
+        }        
+    });
 
-    if (res == "move") {
-        if (flag) {
+    function draw() {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(curX, curY);
+        ctx.strokeStyle = penColour;
+        ctx.lineWidth = penWidth;
+        ctx.stroke();
+        ctx.closePath();
+
+        storeCanvas();
+    }
+    
+    function findCoord(res, e) {
+        if (res == "down") {
             prevX = curX;
             prevY = curY;
             curX = e.clientX - canvas.offsetLeft;
             curY = e.clientY - canvas.offsetTop;
-            draw();
+    
+            flag = true;
+            isDrawing = true;
+            if (isDrawing) {
+                ctx.beginPath();
+                ctx.fillStyle = penColour;
+                ctx.fillRect(curX, curY, 2, 2);
+                ctx.closePath();
+                isDrawing = false;
+            }
+        }
+    
+        if (res == "up" || res == "out") {
+            flag = false;
+        }
+    
+        if (res == "move") {
+            if (flag) {
+                prevX = curX;
+                prevY = curY;
+                curX = e.clientX - canvas.offsetLeft;
+                curY = e.clientY - canvas.offsetTop;
+                draw();
+            }
         }
     }
-}
-
-function storeCanvas(canvasSurface) {
-    try {
-        var canvasDataUrl = canvasSurface[0].toDataURL();
-        localStorage.setItem("canvasDataUrl", canvasDataUrl);
-    } catch (err) {
-        var canvasDataUrl = canvasSurface.toDataURL();
-        console.log(canvasSurface)
+    
+    function storeCanvas() {
+        var canvasDataUrl = canvas.toDataURL();
         localStorage.setItem("canvasDataUrl", canvasDataUrl);
     }
-
-}
-
-function retrieveCanvas(canvas) {
-    var canvasDataUrl = localStorage.getItem("canvasDataUrl");
-
-    var canvasImage = new Image;
-    canvasImage.src = canvasDataUrl;
-    canvasImage.onload = function () {
-        ctx.drawImage(canvasImage, 0, 0);
+    
+    function retrieveCanvas() {
+        var canvasDataUrl = localStorage.getItem("canvasDataUrl");
+    
+        var canvasImage = new Image;
+        canvasImage.src = canvasDataUrl;
+        canvasImage.onload = function () {
+            ctx.drawImage(canvasImage, 0, 0);
+        }
     }
-}
+});
