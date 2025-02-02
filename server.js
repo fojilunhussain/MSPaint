8f59b882331b64c37cc7e10e61989b0718be01e6
Fojilun Hussain <fojilun.hussain@gmail.com>
2025-02-02 23:25:24 +0000

    Get it working after all these years. Updated packages and file structure

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-import { Server as WebSocketServer } from 'ws';
+import pkg from 'ws';
+const { Server: WebSocketServer } = pkg;
+import { createServer } from 'http';
+import express from 'express';
+import path from 'path';
 
-var wss = new WebSocketServer({
-    port: 8080
-});
+const app = express();
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
+
+app.use(express.static(path.resolve('public')));
 
 wss.on("connection", function(ws) {
     ws.on("message", function(message) {
-        console.log("Server received message: %s", message);
+        console.log("Server received message:", message);
 
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === ws.OPEN) {
                 client.send(message);
             }
-        })
+        });
     });
 });
+
+const PORT = 8080;
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
